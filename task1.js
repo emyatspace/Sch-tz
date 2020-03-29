ee0b58426ec7a8f921a51ffbb14f8e8b12667154
emyatspace <61155725+emyatspace@users.noreply.github.com>
2020-03-28 23:31:31 -0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,19 @@
+<script src="task1.js"></script>
+var name = "Emeka";
+console.log("My name is: " + name);
+
+var courses = ["Html", "Css", "Javascript", "Python", "Nodejs", "Kotlin"];
+
+const data = [];
+
+ for (let i=0; i<=100; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+}
+
+console.log(data);
+
